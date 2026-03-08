9e882814a30db37abcafc3e4f855c1b385a92bf7
Neeraja Abhyankar <neeraja.abhyankar@gmail.com>
2026-03-07 19:37:01 -0600

    feat: cleanup + unit test for videos

--- plugins/label_propagation/propagation.py
+++ plugins/label_propagation/propagation.py
@@ -1,3 +1,4 @@
+import numpy as np
 import logging
 from typing import Tuple, Union, Optional, Any, List
 
@@ -34,30 +35,17 @@ def propagate_annotations_sam2(
         media_mode="image",
     )
 
-    if view.has_field("tags"):
-        all_tags = view.distinct("tags") or []
-        sequence_tags = [t for t in all_tags if t != "val"]
-        for tag in sequence_tags:
-            seq_view = view.match_tags(tag)
-            if sort_field and view.has_field(sort_field):
-                seq_view = seq_view.sort_by(sort_field)
-            seq_view.apply_model(
-                model,
-                label_field=output_annotation_field,
-                prompt_field=input_annotation_field,
-                progress=progress,
-            )
-    else:
-        run_view = (
-            view.sort_by(sort_field)
-            if (sort_field and view.has_field(sort_field))
-            else view
-        )
-        run_view.apply_model(
-            model,
-            label_field=output_annotation_field,
-            prompt_field=input_annotation_field,
-            progress=progress,
-        )
+    run_view = (
+        view.sort_by(sort_field)
+        if (sort_field and view.has_field(sort_field))
+        else view
+    )
+    run_view.apply_model(
+        model,
+        label_field=output_annotation_field,
+        prompt_field=input_annotation_field,
+        batch_size=int(2**np.ceil(np.log2(len(run_view)))),  # type: ignore[arg-type]
+        progress=progress,
+    )
 
     return {}
--- plugins/label_propagation/tests/intensive/test_davis.py
+++ plugins/label_propagation/tests/intensive/test_davis.py
@@ -10,11 +10,9 @@ import fiftyone.zoo as foz
 import fiftyone.operators as foo
 from fiftyone.core.expressions import ViewField as F
 
-# from ..utils import evaluate
-
 
 @pytest.fixture
-def dataset_view():
+def image_dataset_view():
     dataset = foz.load_zoo_dataset(
         "https://github.com/voxel51/davis-2017",
         split="validation",
@@ -27,29 +25,28 @@ def dataset_view():
 
 
 @pytest.fixture
-def partially_labeled_dataset_view(dataset_view):
-    if "labels_test" in dataset_view._dataset.get_field_schema():
-        try:
-            dataset_view._dataset.delete_sample_field(
-                "labels_test", error_level=2
-            )
-        except AttributeError:
-            assert (
-                "labels_test" not in dataset_view._dataset.get_field_schema()
-            ), "Unable to delete labels_test field"
+def partially_labeled_image_dataset_view(image_dataset_view):
+    if "labels_test" in image_dataset_view._dataset.get_field_schema():
+        image_dataset_view._dataset.delete_sample_field(
+            "labels_test", error_level=2
+        )
+    if "labels_test_propagated" in image_dataset_view._dataset.get_field_schema():
+        image_dataset_view._dataset.delete_sample_field(
+            "labels_test_propagated", error_level=2
+        )
 
-    if "labels_test" not in dataset_view._dataset.get_field_schema():
-        dataset_view._dataset.add_sample_field(
+    if "labels_test" not in image_dataset_view._dataset.get_field_schema():
+        image_dataset_view._dataset.add_sample_field(
             "labels_test",
             fo.EmbeddedDocumentField,
             embedded_doc_type=fo.Detections,
         )
 
-    sequences = dataset_view.distinct("tags")
+    sequences = image_dataset_view.distinct("tags")
     sequences.remove("val")
     new_frame_number = 0
     for seq in sequences:
-        seq_slice = dataset_view.match_tags(seq).sort_by("frame_number")
+        seq_slice = image_dataset_view.match_tags(seq).sort_by("frame_number")
         seq_slice.set_values(
             "new_frame_number",
             [new_frame_number + ii for ii in range(len(seq_slice))],
@@ -62,13 +59,42 @@ def partially_labeled_dataset_view(dataset_view):
         exemplar_sample["labels_test"] = exemplar_sample["ground_truth"]
         exemplar_sample.save()
 
-    return dataset_view
+    return image_dataset_view
 
 
-def test_assign_exemplar_frames(dataset_view):
+@pytest.fixture
+def video_dataset_view():
+    dataset = foz.load_zoo_dataset("quickstart-video").limit(1)
+    dataset.match_frames(F("frame_number") <= 4).keep_frames()
+    return dataset
+
+
+@pytest.fixture
+def partially_labeled_video_dataset_view(video_dataset_view):
+    if "labels_test" not in video_dataset_view._dataset.get_frame_field_schema():
+        video_dataset_view._dataset.add_frame_field(
+            "labels_test",
+            fo.EmbeddedDocumentField,
+            embedded_doc_type=fo.Detections,
+        )
+    (
+        video_dataset_view.match_frames(F("frame_number") > 1)
+        .set_field("frames.labels_test", fo.Detections(detections=[]))
+        .save()
+    )
+    (
+        video_dataset_view.match_frames(F("frame_number") == 1)
+        .set_field("frames.labels_test", F("detections"))
+        .save()
+    )
+
+    return video_dataset_view
+
+
+def test_assign_exemplar_frames(image_dataset_view):
     ctx = {
-        "dataset": dataset_view._dataset,
-        "view": dataset_view,
+        "dataset": image_dataset_view._dataset,
+        "view": image_dataset_view,
         "params": {
             "selection_method": "heuristic",
             "sort_field": "frame_number",
@@ -81,15 +107,15 @@ def test_assign_exemplar_frames(dataset_view):
     )
     print(result.result["message"])  # type: ignore[index]
 
-    exemplars = dataset_view.values("exemplar_test.is_exemplar")
+    exemplars = image_dataset_view.values("exemplar_test.is_exemplar")
     assert exemplars[0]
     assert np.mean(exemplars) < 0.33
 
 
-def test_propagate_labels(partially_labeled_dataset_view):
+def test_propagate_labels_image(partially_labeled_image_dataset_view):
     ctx = {
-        "dataset": partially_labeled_dataset_view._dataset,
-        "view": partially_labeled_dataset_view,
+        "dataset": partially_labeled_image_dataset_view._dataset,
+        "view": partially_labeled_image_dataset_view,
         "params": {
             "input_annotation_field": "labels_test",
             "output_annotation_field": "labels_test_propagated",
@@ -110,10 +136,54 @@ def test_propagate_labels(partially_labeled_dataset_view):
     )
     areas = [
         sum([detection_area(det) for det in prop])
-        for prop in partially_labeled_dataset_view.values(
+        for prop in partially_labeled_image_dataset_view.values(
             "labels_test_propagated.detections"
         )
     ]
     assert np.min(areas) > 0.35
+
+    # TODO(neeraja): add evaluation [in a follow-up PR]
+
+    indices = partially_labeled_image_dataset_view.values(
+        "labels_test_propagated.detections.index"
+    )
+    assert indices[0] == indices[-1]  # same number of objects in the first and last frames
+
+
+def test_propagate_labels_video(partially_labeled_video_dataset_view):
+    assert len(partially_labeled_video_dataset_view) == 1
+    sample = partially_labeled_video_dataset_view.first()
+    assert len(sample.frames) == 4
+    frame = sample.frames[1]
+    assert len(frame["labels_test"].detections) > 0
+    frame = sample.frames[2]
+    assert len(frame["labels_test"].detections) == 0
+    ctx = {
+        "dataset": partially_labeled_video_dataset_view._dataset,
+        "view": partially_labeled_video_dataset_view,
+        "params": {
+            "input_annotation_field": "frames.labels_test",
+            "output_annotation_field": "frames.labels_test_propagated",
+            "propagation_method": "sam2",
+            "sort_field": "frames.frame_number",
+        },
+    }
+    result = foo.execute_operator(
+        "@51labs/label_propagation/propagate_labels", ctx
+    )
+    print(result.result["message"])  # type: ignore[index]
+
+    detection_area = (
+        lambda det: (det.bounding_box[2] * det.bounding_box[3])
+        if det.bounding_box is not None
+        else 0
+    )
+    areas = [
+        sum([detection_area(det) for det in prop])
+        for prop in partially_labeled_video_dataset_view.values(
+            "frames.labels_test_propagated.detections"
+        )
+    ]
+    assert np.min(areas) > 0.1
 
     # TODO(neeraja): add evaluation [in a follow-up PR]
